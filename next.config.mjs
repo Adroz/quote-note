772b5fe759e8f808938ed56d22a9bd8c14939de2
Nik Moores <me@nikmoores.com>
2025-03-21 12:33:31 +1000

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'build',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/quote-note' : '',
+  // This setting may be needed if your repo name doesn't match the base path
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/quote-note/' : '',
 };
 
 export default nextConfig; 
